7374810110ce5736dbd987d1c77ee9bcdbe7570c
MateuszXiaomi <mt.sulek@gmail.com>
2018-06-05 10:31:27 +0200

    cleaning

--- KomputerowePO/zaj8/zaj8.1.m
+++
@@ -1,7 +0,0 @@
-L1=imread('mikolaj.jpg');
-figure;
-imshow(L1);
-level = graythresh(L1);
-L2 = im2bw(L1,level);
-figure;
-imshow(L2);
--- MechanikaMikroświata/ech.m
+++
@@ -1,7 +0,0 @@
-% WTF
-
-syms a b c x
-
-wynik = solve('a*x^2 + b*x + c = 0'); 
-
-pretty(wynik)
